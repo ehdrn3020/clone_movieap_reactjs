44c5f884bb8bba3a14eefa4951d9faec36ee710f
ehdrn3020 <ehdrnsla3020@naver.com>
2022-11-07 07:23:46 +0900

    20221107 add App.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import { useState, useEffect } from 'react';
 
+import SearchIcon from './search.svg'
+import './App.css';
+
+const API_URL = 'http://www.omdbapi.com?apikey=a46217e7';
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const searchText = document.querySelector(".search input");
+  searchText.addEventListener("change", ()=>{console.log(123);}
+  );
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+  }
+
+  useEffect(()=>{
+    searchMovies('Super');
+  },[]);
 
   return (
-      <h1>App</h1>
+      <div className="app">
+        <h1>Movie Search</h1>
+
+        <div className="search">
+          <input 
+            value={searchTerm}
+            placeholder="Search for movies"
+          />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+        </div>
+      </div>
   );
 }
 export default App;
